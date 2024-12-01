4a08f6363ba14d550602176c60807bb1d2c40b05
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-12-01 15:49:02 +0500

    fe

--- my-app/app/components/sessionwrapper.tsx
+++
@@ -1,8 +0,0 @@
-// components/sessionwrapper.tsx
-
-'use client'
-import { SessionProvider} from 'next-auth/react';
-
-export const SessionWrapper = ({children}:{children:React.ReactNode})=>{
-    return <SessionProvider>{children}</SessionProvider>
-}
